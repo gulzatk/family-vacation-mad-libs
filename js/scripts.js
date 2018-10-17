ad8d123cc78350f46fd1f665a6ce1b5ce3d7f9ef
Daniel Lira and Gulzat Karimova <daniel@>
2018-10-17 09:48:02 -0700

    Createdscripts for user input and output

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-$(document).ready(function() {
-  $("#formOne").submit(function() {
-    $(".person1").text("blah blah");
-    $(".person2").text("blah blah");
-    $(".animal").text("blah blah");
-    $(".exclamation").text("blah blah");
-    $(".verb").text("blah blah");
 
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var lastnameInput = $("input#last-name").val();
+    var locationInput = $("input#location").val();
+    var activityInput= $("input#activity").val();
+    var foodInput = $("input#food").val();
+    var adjectiveInput = $("input#adjective").val();
+
+    $(".last-name").text(lastnameInput);
+    $(".location").text(locationInput);
+    $(".activity").text(activityInput);
+    $(".food").text(foodInput);
+    $(".adjective").text(adjectiveInput);
 
     $("#story").show();
+
+    event.preventDefault();
   });
-})
+});
