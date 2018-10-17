0fabea6cc48c917d438389a97c04aee1854090cb
Daniel Lira and Gulzat Karimova <gulzat@>
2018-10-17 09:15:41 -0700

    Setup html, css, js, created mad-libs file

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    $(".person1").text("blah blah");
+    $(".person2").text("blah blah");
+    $(".animal").text("blah blah");
+    $(".exclamation").text("blah blah");
+    $(".verb").text("blah blah");
+
+
+    $("#story").show();
+  });
+})
